89309d7cb585186c642fd65a783d6627b5fbe1b4
Jared Peter <aljapeter05@gmail.com>
2025-12-24 10:16:14 -0300

    added get by id query for exam

---
+++ Application/Features/ExamFeature/Queries/GetExamByIdQuery/GetExamByIdHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SetelaServerV3._1.Application.Features.ExamFeature.DTO;
+using SetelaServerV3._1.Infrastructure.Data;
+using SetelaServerV3._1.Infrastructure.Extensions;
+using SetelaServerV3._1.Shared.Utilities;
+
+namespace SetelaServerV3._1.Application.Features.ExamFeature.Queries.GetExamByIdQuery
+{
+    public class GetExamByIdHandler(AppDbContext _db, IMapper _mapper) : IRequestHandler<GetExamByIdQuery, Result<ExamDTO>>
+    {
+        public async Task<Result<ExamDTO>> Handle(GetExamByIdQuery query, CancellationToken cancellationToken)
+        {
+            var exam = await _db.Exams
+                .ProjectTo<ExamDTO>(_mapper.ConfigurationProvider)
+                .FirstOrDefaultAsync(e => e.Id == query.ExamId, cancellationToken)
+                .LoadResources(_db, _mapper, Domain.Enums.ResourceParentType.Exam, cancellationToken);
+
+            if (exam == null)
+                return Result<ExamDTO>.Fail("El examen no existe.", 404);
+
+            return Result<ExamDTO>.Ok(exam);
+        }
+    }
+}
---
+++ Application/Features/ExamFeature/Queries/GetExamByIdQuery/GetExamByIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using SetelaServerV3._1.Application.Features.ExamFeature.DTO;
+using SetelaServerV3._1.Shared.Utilities;
+
+namespace SetelaServerV3._1.Application.Features.ExamFeature.Queries.GetExamByIdQuery
+{
+    public class GetExamByIdQuery : IRequest<Result<ExamDTO>>
+    {
+        public int ExamId { get; set; }
+    }
+}
