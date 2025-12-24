1edbd8275ebe8ffe7bb7f48e25acaf516febed15
Jared Peter <aljapeter05@gmail.com>
2025-12-24 10:11:49 -0300

    added delete command

---
+++ Application/Features/ExamFeature/Commands/DeleteExamCommand/DeleteExamCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using SetelaServerV3._1.Shared.Utilities;
+
+namespace SetelaServerV3._1.Application.Features.ExamFeature.Commands.DeleteExamCommand
+{
+    public class DeleteExamCommand : IRequest<Result<object>>
+    {
+        public int UserId { get; set; }
+        public int ExamId { get; set; }
+    }
+}
---
+++ Application/Features/ExamFeature/Commands/DeleteExamCommand/DeleteExamHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+using SetelaServerV3._1.Application.Features.ExamFeature.DTO;
+using SetelaServerV3._1.Infrastructure.Data;
+using SetelaServerV3._1.Shared.Policies;
+using SetelaServerV3._1.Shared.Utilities;
+
+namespace SetelaServerV3._1.Application.Features.ExamFeature.Commands.DeleteExamCommand
+{
+    public class DeleteExamHandler(AppDbContext _db, IPermissionHandler _userPermissions) : IRequestHandler<DeleteExamCommand, Result<object>>
+    {
+        public async Task<Result<object>> Handle(DeleteExamCommand command, CancellationToken cancellationToken)
+        {
+            var exam = await _db.Exams.FindAsync([command.ExamId], cancellationToken);
+            if (exam == null) return Result<object>.Fail("El examen no existe.");
+
+            if (!await _userPermissions.CanEditCourse(command.UserId, exam.CourseId))
+                return Result<object>.Fail("No tiene permisos para editar examenes.");
+
+            _db.Exams.Remove(exam);
+            await _db.SaveChangesAsync(cancellationToken);
+
+            return Result<object>.Ok(new { Success = true });
+        }
+    }
+}
