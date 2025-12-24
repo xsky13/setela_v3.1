7057d71d72584bd8776230c0c5423613fa5732d3
Jared Peter <aljapeter05@gmail.com>
2025-12-24 14:54:44 -0300

    added delete command

--- Application/Features/ExamSubmissionFeature/Commands/DeleteExamSubmissionCommand/DeleteExamSubmissionHandler.cs
+++ Application/Features/ExamSubmissionFeature/Commands/DeleteExamSubmissionCommand/DeleteExamSubmissionHandler.cs
@@ -1,18 +1,29 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using SetelaServerV3._1.Infrastructure.Data;
+using SetelaServerV3._1.Shared.Policies;
 using SetelaServerV3._1.Shared.Utilities;
 
 namespace SetelaServerV3._1.Application.Features.ExamSubmissionFeature.Commands.DeleteExamSubmissionCommand
 {
-    public class DeleteExamSubmissionHandler(AppDbContext _db) : IRequestHandler<DeleteExamSubmissionCommand, Result<object>>
+    public class DeleteExamSubmissionHandler(AppDbContext _db, IPermissionHandler _userPermissions) : IRequestHandler<DeleteExamSubmissionCommand, Result<object>>
     {
         public async Task<Result<object>> Handle(DeleteExamSubmissionCommand command, CancellationToken cancellationToken)
         {
-            var submission = await _db.ExamSubmissions.FindAsync([command.ExamSubmissionId], cancellationToken);
+            var submission = await _db.ExamSubmissions
+                .Where(e => e.Id == command.ExamSubmissionId)
+                .Select(e => new { e.SysUserId, e.Exam.CourseId })
+                .FirstOrDefaultAsync(cancellationToken);
             if (submission == null) return Result<object>.Fail("La entrega no existe");
 
-            _db.ExamSubmissions.Remove(submission);
-            await _db.SaveChangesAsync(cancellationToken);
+            var canEditCourse = await _userPermissions.CanEditCourse(command.UserId, submission.CourseId);
+
+            if (submission.SysUserId != command.UserId && !canEditCourse)
+                return Result<object>.Fail("No puede borrar esta entrega", 403);
+
+            await _db.ExamSubmissions
+                .Where(e => e.Id == command.ExamSubmissionId)
+                .ExecuteDeleteAsync(cancellationToken);
 
             return Result<object>.Ok(new { Success = true });
         }
