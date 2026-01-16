c71dcc12d004009a2a3a04611fa2c698fc34b84b
Jared Peter <aljapeter05@gmail.com>
2026-01-16 15:43:21 -0300

    changes to resource dto

--- Shared/Common/DTO/CourseResourceDTO.cs
+++ Shared/Common/DTO/CourseResourceDTO.cs
@@ -1,10 +1,13 @@
-﻿namespace SetelaServerV3._1.Shared.Common.DTO
+﻿using SetelaServerV3._1.Domain.Enums;
+
+namespace SetelaServerV3._1.Shared.Common.DTO
 {
     public class CourseResourceDTO
     {
         public int Id { get; set; }
         public string Url { get; set; }
         public string? LinkText { get; set; }
+        public ResourceType ResourceType { get; set; }
         public DateTime CreationDate { get; set; }
         public UserSimpleDTO SysUser { get; set; }
         public int DisplayOrder { get; set; }
