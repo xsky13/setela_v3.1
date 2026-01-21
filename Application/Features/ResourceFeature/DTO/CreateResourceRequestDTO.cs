d94ac0090f1f0db0860316302e5a56b0a67f5002
Jared Peter <aljapeter05@gmail.com>
2026-01-21 11:18:20 -0300

    minor changes to dto

--- Application/Features/ResourceFeature/DTO/CreateResourceRequestDTO.cs
+++ Application/Features/ResourceFeature/DTO/CreateResourceRequestDTO.cs
@@ -1,7 +1,10 @@
-﻿namespace SetelaServerV3._1.Application.Features.ResourceFeature.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SetelaServerV3._1.Application.Features.ResourceFeature.DTO
 {
     public class CreateResourceRequestDTO
     {
+        [Required, MinLength(1)]
         public string Url { get; set; }
         public string? LinkText { get; set; }
         public string Type { get; set; }
--- Shared/Common/DTO/CourseResourceDTO.cs
+++ Shared/Common/DTO/CourseResourceDTO.cs
@@ -6,8 +6,10 @@ namespace SetelaServerV3._1.Shared.Common.DTO
     {
         public int Id { get; set; }
         public string Url { get; set; }
-        public string? LinkText { get; set; }
+        public string? LinkText { get; set; } 
         public ResourceType ResourceType { get; set; }
+        public ResourceParentType ParentType { get; set; }
+        public int ParentId { get; set; }
         public DateTime CreationDate { get; set; }
         public UserSimpleDTO SysUser { get; set; }
         public int DisplayOrder { get; set; }
