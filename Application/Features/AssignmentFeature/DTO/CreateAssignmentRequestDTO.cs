bc93d9d20ed1fd1d9398ed3926589f8acbe20d12
Jared Peter <aljapeter05@gmail.com>
2026-01-26 16:21:31 -0300

    added required fields when creating assignments

--- Application/Features/AssignmentFeature/DTO/CreateAssignmentRequestDTO.cs
+++ Application/Features/AssignmentFeature/DTO/CreateAssignmentRequestDTO.cs
@@ -1,10 +1,14 @@
-﻿namespace SetelaServerV3._1.Application.Features.AssignmentFeature.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SetelaServerV3._1.Application.Features.AssignmentFeature.DTO
 {
     public class CreateAssignmentRequestDTO
     {
         public int CourseId { get; set; }
+        [Required(ErrorMessage = "El titulo no puede estar vacío."), MinLength(1, ErrorMessage = "El titulo no puede estar vacío.")]
         public string Title { get; set; }
-        public string? TextContent { get; set; }
+        [Required(ErrorMessage = "El contenido no puede estar vacío."), MinLength(1, ErrorMessage = "El contenido no puede estar vacío.")]
+        public string TextContent { get; set; }
         public int MaxGrade { get; set; }
         public int Weight { get; set; }
         public DateTime DueDate { get; set; }
