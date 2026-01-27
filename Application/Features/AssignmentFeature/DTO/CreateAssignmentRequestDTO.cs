c41b624cedddc6bf5aca5ac8b1f40e2d8d21cd0f
Jared Peter <aljapeter05@gmail.com>
2026-01-26 22:09:02 -0300

    more changes to requirements for dtos

--- Application/Features/AssignmentFeature/DTO/CreateAssignmentRequestDTO.cs
+++ Application/Features/AssignmentFeature/DTO/CreateAssignmentRequestDTO.cs
@@ -9,8 +9,11 @@ namespace SetelaServerV3._1.Application.Features.AssignmentFeature.DTO
         public string Title { get; set; }
         [Required(ErrorMessage = "El contenido no puede estar vacío."), MinLength(1, ErrorMessage = "El contenido no puede estar vacío.")]
         public string TextContent { get; set; }
+        [Required(ErrorMessage = "La nota maxima no puede estar vacío.")]
         public int MaxGrade { get; set; }
+        [Required(ErrorMessage = "El peso de la nota no puede estar vacío.")]
         public int Weight { get; set; }
+        [Required(ErrorMessage = "La fecha de entrega no puede estar vacía."),]
         public DateTime DueDate { get; set; }
     }
 }
