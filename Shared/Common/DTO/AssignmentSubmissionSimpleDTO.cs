630705eb0d0f82bb36be4c902f69b97b2f29f16c
Jared Peter <aljapeter05@gmail.com>
2026-02-03 10:26:06 -0300

    added assignment id for assignment submission dto

--- Shared/Common/DTO/AssignmentSubmissionSimpleDTO.cs
+++ Shared/Common/DTO/AssignmentSubmissionSimpleDTO.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public int SysUserId { get; set; }
         public UserSimpleDTO SysUser { get; set; }
+        public int AssignmentId { get; set; }
         public DateTime CreationDate { get; set; }
         public DateTime LastUpdateDate { get; set; }
         public int? Grade { get; set; }
