a0a1253f5a480a5b927279304ea6b729fd04e835
Jared Peter <aljapeter05@gmail.com>
2026-02-23 19:15:18 -0300

    added progress model

---
+++ Domain/Entities/UserProgress.cs
@@ -0,0 +1,15 @@
+﻿using SetelaServerV3._1.Domain.Enums;
+
+namespace SetelaServerV3._1.Domain.Entities
+{
+    public class UserProgress
+    {
+        public int Id { get; set; }
+        public ProgressParentType ParentType { get; set; }
+        public int ParentId { get; set; }
+        public int SysUserId { get; set; }
+        public SysUser SysUser { get; set; }
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+    }
+}
---
+++ Domain/Enums/ProgressParentType.cs
@@ -0,0 +1,9 @@
+﻿namespace SetelaServerV3._1.Domain.Enums
+{
+    public enum ProgressParentType
+    {
+        Module = 1,
+        Assignment = 2,
+        Exam = 3,
+    }
+}
