4ed4a68e999a808df1f9fe0875c61137e0da3a03
Jared Peter <aljapeter05@gmail.com>
2026-02-23 20:46:41 -0300

    started porcentage command

---
+++ Application/Features/UserProgressFeature/GetProgressCommand/GetProgressCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using SetelaServerV3._1.Shared.Utilities;
+
+namespace SetelaServerV3._1.Application.Features.UserProgressFeature.GetProgressCommand
+{
+    public class GetProgressCommand : IRequest<Result<int>>
+    {
+        public int UserId { get; set; }
+        public int CourseId { get; set; }
+    }
+}
---
+++ Application/Features/UserProgressFeature/GetProgressCommand/GetProgressHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using SetelaServerV3._1.Infrastructure.Data;
+using SetelaServerV3._1.Shared.Utilities;
+
+namespace SetelaServerV3._1.Application.Features.UserProgressFeature.GetProgressCommand
+{
+    public class GetProgressHandler(AppDbContext _db) : IRequestHandler<GetProgressCommand, Result<int>>
+    {
+        public async Task<Result<int>> Handle(GetProgressCommand command, CancellationToken cancellationToken)
+        {
+
+            throw new NotImplementedException();
+        }
+    }
+}
