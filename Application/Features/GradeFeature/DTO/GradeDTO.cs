a3b0bb3d1d71d14a53db0c0824c0f58841c7e06d
Jared Peter <aljapeter05@gmail.com>
2026-02-25 13:06:25 -0300

    added grade list fetching

--- Application/Features/GradeFeature/DTO/GradeDTO.cs
+++ Application/Features/GradeFeature/DTO/GradeDTO.cs
@@ -4,12 +4,20 @@ using SetelaServerV3._1.Shared.Common.DTO;
 
 namespace SetelaServerV3._1.Application.Features.GradeFeature.DTO
 {
+    public class ParentHelper
+    {
+        public string ItemTitle { get; set; }
+        public int MaxGrade { get; set; }
+        public int GrandParentId { get; set; }
+    };
+
     public class GradeDTO
     {
         public int Id { get; set; }
         public int Value { get; set; }
         public GradeParentType ParentType { get; set; }
         public int ParentId { get; set; }
+        public ParentHelper? ParentHelper { get; set; }
         public int SysUserId { get; set; }
         public UserSimpleDTO SysUser { get; set; }
         public int CourseId { get; set; }
--- Application/Features/GradeFeature/Queries/GetGradesForStudentQuery/GetGradesForStudentHandler.cs
+++ Application/Features/GradeFeature/Queries/GetGradesForStudentQuery/GetGradesForStudentHandler.cs
@@ -1,21 +1,45 @@
-﻿using AutoMapper;
-using AutoMapper.QueryableExtensions;
-using MediatR;
+﻿using MediatR;
 using Microsoft.EntityFrameworkCore;
 using SetelaServerV3._1.Application.Features.GradeFeature.DTO;
+using SetelaServerV3._1.Domain.Enums;
 using SetelaServerV3._1.Infrastructure.Data;
 using SetelaServerV3._1.Shared.Utilities;
 
 namespace SetelaServerV3._1.Application.Features.GradeFeature.Queries.GetGradesForStudentQuery
 {
-    public class GetGradesForStudentHandler(AppDbContext _db, IMapper _mapper) : IRequestHandler<GetGradesForStudentQuery, Result<List<GradeDTO>>>
+    public class GetGradesForStudentHandler(AppDbContext _db) : IRequestHandler<GetGradesForStudentQuery, Result<List<GradeDTO>>>
     {
         public async Task<Result<List<GradeDTO>>> Handle(GetGradesForStudentQuery query, CancellationToken cancellationToken)
         {
             var gradeList = await _db.Grades
-                .ProjectTo<GradeDTO>(_mapper.ConfigurationProvider)
                 .Where(g => g.CourseId == query.CourseId && g.SysUserId == query.UserId)
+                .Select(g => new GradeDTO
+                {
+                    Id = g.Id,
+                    Value = g.Value,
+                    ParentType = g.ParentType,
+                    ParentId = g.ParentId,
+                    CourseId = g.CourseId,
+                    ParentHelper = g.ParentType == GradeParentType.AssignmentSubmission
+                        ? _db.AssignmentSubmissions
+                            .Where(s => s.Id == g.ParentId)
+                            .Select(s => new ParentHelper
+                            {
+                                ItemTitle = s.Assignment.Title,
+                                MaxGrade = s.Assignment.MaxGrade,
+                                GrandParentId = s.AssignmentId
+                            }).FirstOrDefault()
+                        : _db.ExamSubmissions
+                            .Where(s => s.Id == g.ParentId)
+                            .Select(s => new ParentHelper
+                            {
+                                ItemTitle = s.Exam.Title,
+                                MaxGrade = s.Exam.MaxGrade,
+                                GrandParentId = s.ExamId
+                            }).FirstOrDefault()
+                })
                 .ToListAsync(cancellationToken);
+
 
             return Result<List<GradeDTO>>.Ok(gradeList);
         }
