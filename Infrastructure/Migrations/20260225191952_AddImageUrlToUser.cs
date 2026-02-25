2689c90b7fbc6d8d44df273cd1ed0ecc28335f79
Jared Peter <aljapeter05@gmail.com>
2026-02-25 16:20:43 -0300

    added user url to database

---
+++ Infrastructure/Migrations/20260225191952_AddImageUrlToUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SetelaServerV3._1.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageUrlToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserImageUrl",
+                table: "SysUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserImageUrl",
+                table: "SysUsers");
+        }
+    }
+}
