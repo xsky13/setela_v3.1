b5797565c85e52a02f32cc298bd854a27fc4d739
Jared Peter <aljapeter05@gmail.com>
2026-02-27 08:16:12 -0300

    added change password command

---
+++ Application/Features/UserFeature/Commands/ChangePasswordCommand/ChangePasswordCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using SetelaServerV3._1.Shared.Utilities;
+using System.ComponentModel.DataAnnotations;
+
+namespace SetelaServerV3._1.Application.Features.UserFeature.Commands.ChangePasswordCommand
+{
+    public class ChangePasswordCommand : IRequest<Result<object>>
+    {
+        [Required(ErrorMessage = "No puede haber campos vacios.")]
+        public string OldPassword { get; set; }
+        [Required(ErrorMessage ="No puede haber campos vacios."), MinLength(6, ErrorMessage ="La contraseña debe tener por lo menos 6 caracteres.")]
+        public string NewPassword { get; set; }
+        [Required(ErrorMessage = "No puede haber campos vacios.")]
+        public string NewPasswordRepeat { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ Application/Features/UserFeature/Commands/ChangePasswordCommand/ChangePasswordHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using SetelaServerV3._1.Infrastructure.Data;
+using SetelaServerV3._1.Shared.Utilities;
+
+namespace SetelaServerV3._1.Application.Features.UserFeature.Commands.ChangePasswordCommand
+{
+    public class ChangePasswordHandler(AppDbContext _db) : IRequestHandler<ChangePasswordCommand, Result<object>>
+    {
+        public async Task<Result<object>> Handle(ChangePasswordCommand command, CancellationToken cancellationToken)
+        {
+            var user = await _db.SysUsers.FindAsync([command.UserId], cancellationToken);
+            if (user == null) return Result<object>.Fail("El usuario no existe");
+
+            if (!BCrypt.Net.BCrypt.Verify(command.OldPassword, user.PasswordHash))
+                return Result<object>.Fail("La contraseña anterior no es correcta");
+
+            if (command.NewPassword != command.NewPasswordRepeat)
+                return Result<object>.Fail("Las contraseñas no son iguales");
+
+            user.PasswordHash = BCrypt.Net.BCrypt.HashPassword(command.NewPassword);
+
+            await _db.SaveChangesAsync(cancellationToken);
+            return Result<object>.Ok(new { Success = true });
+        }
+    }
+}
